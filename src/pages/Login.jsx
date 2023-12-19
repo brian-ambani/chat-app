c007f155f49e76a4ff89ee8be7a0ef744b48b948
brian-ambani <brainambani1@gmail.com>
2023-12-20 02:22:17 +0300

    updated the Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Add from "../img/addAvatar.png";
 
-const Login = () => {
-  return <div>Login</div>;
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Brain Chat</span>
+        <span className="title">Register</span>
+        <form action="">
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="addAvatar" /> <span>Add Avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default Register;
