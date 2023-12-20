cc1fbf3ee81e00f93c785911e80dc2f98cd01010
brian-ambani <brainambani1@gmail.com>
2023-12-20 12:31:19 +0300

    updated the Messages component

---
+++ src/components/Messages.jsx
@@ -0,0 +1,23 @@
+import Message from "../components/Message";
+
+const Messages = () => {
+  return (
+    <div className="messages">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
