33b09ea82d8ddc1e15f266bde8a10fb7f2ee9900
brian-ambani <brainambani1@gmail.com>
2023-12-20 12:31:52 +0300

    updated the Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/3828245/pexels-photo-3828245.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/3828245/pexels-photo-3828245.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
